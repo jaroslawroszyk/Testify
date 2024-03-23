b47e5496de6a0d2db4e4bfb670ad7f7904aa624a
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2024-03-23 16:42:37 +0100

    refactor: Move js and css to static folder

---
+++ testify/static/js/setTimeout.js
@@ -0,0 +1,6 @@
+timeoutTime = 1000;
+
+setTimeout(function () {
+    document.getElementById('video-container').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}, timeoutTime);
